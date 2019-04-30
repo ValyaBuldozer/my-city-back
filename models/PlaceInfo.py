e092f33dba7a049862256feea618b29a24041343
Vladislav Kotelnikov <kiselek44@yandex.ru>
2019-04-30 18:42:05 +0300

    Some minor changes at server.

--- models/PlaceInfo.py
+++ models/PlaceInfo.py
@@ -4,4 +4,6 @@ class PlaceInfo:
         self.place_id = db_tuple[0]
         self.place_name = db_tuple[1]
         self.place_logo_path = db_tuple[2]
+        self.place_lat = str(db_tuple[3]) if db_tuple[3] is not None else None
+        self.place_lng = str(db_tuple[4]) if db_tuple[4] is not None else None
 
