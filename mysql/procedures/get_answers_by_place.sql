3db36a2345c00fb87d19b3d030ad20f92bf299b0
ValyaBuldozer <kiselek44@yandex.ru>
2019-05-04 19:22:16 +0300

    Fix answers procedure

--- mysql/procedures/get_answers_by_place.sql
+++ mysql/procedures/get_answers_by_place.sql
@@ -2,7 +2,7 @@ DELIMITER //
 DROP PROCEDURE IF EXISTS get_answers_by_place;
 CREATE PROCEDURE get_answers_by_place(IN target_place_id INT)
 BEGIN 
-	SELECT *
+	SELECT answer_id, answer_title, answer_is_right, answer_description
     FROM answer
     WHERE place_id = target_place_id;
 END;
